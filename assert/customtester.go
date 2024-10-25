d7e6ddc594b2f250c5bb88fe3c65e79941dafdaf
pman <pman-$-432894328923@gmail.com>
2024-10-26 00:00:53 +0200

    ...

---
+++ assert/customtester.go
@@ -0,0 +1,22 @@
+package assert
+
+import "testing"
+
+func Equal[V comparable](t *testing.T, got, expected V) {
+    t.Helper()
+
+    if expected != got {
+        t.Fatalf(`
+🔵 Expected:
+%v
+
+🔴 Got:
+%v
+
+`, expected, got)
+    }
+}
+
+// Usage:
+//
+// assert.Equal(t, response, `{"status":"ok"}`)
