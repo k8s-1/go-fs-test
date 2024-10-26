9a5d490850151ff023fa8028cce37dcc1379339b
pman <pman-$-432894328923@gmail.com>
2024-10-26 08:59:52 +0200

    chore: update file(s): main_test.go

--- main_test.go
+++ main_test.go
@@ -5,11 +5,9 @@ import (
 )
 
 func TestProcessFile(t *testing.T) {
-	// Define the test base path and environment
 	basePath := "testdata"
 	env := "dev"
 
-	// Call ProcessFile using the testdata directory
 	err := ProcessFile(basePath, env)
 	if err != nil {
 		t.Fatalf("ProcessFile returned an error: %v", err)
