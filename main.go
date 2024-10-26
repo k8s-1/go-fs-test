d74015c52fb5b40045f5310b826986c61f9284ad
pman <pman-$-432894328923@gmail.com>
2024-10-26 08:52:08 +0200

    ...

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"testing"
 )
 
 const BasePath = "/app/config"
@@ -47,30 +46,4 @@ func ProcessFile(basePath, environment string) error {
 	fmt.Println(s)
 
 	return nil
-}
-
-// Now, you can pass a mock path or a temporary test directory in your unit tests.
-func TestProcessFile(t *testing.T) {
-	// Create a temporary directory
-	tempDir, err := os.MkdirTemp(".", "test")
-	if err != nil {
-		t.Fatalf("Failed to create temp dir: %v", err)
-	}
-	defer os.RemoveAll(tempDir) // Clean up after the test
-
-	// Define environment and file path
-	env := "dev"
-	filePath := fmt.Sprintf("%s/%s.yaml", tempDir, env)
-
-	// Write test data to the temporary file
-	content := []byte("hello, world")
-	if err := os.WriteFile(filePath, content, 0o660); err != nil {
-		t.Fatalf("Failed to write test file: %v", err)
-	}
-
-	// Call the ProcessFile function with the temporary directory as basePath
-	err = ProcessFile(tempDir, env)
-	if err != nil {
-		t.Fatalf("ProcessFile returned an error: %v", err)
-	}
 }
